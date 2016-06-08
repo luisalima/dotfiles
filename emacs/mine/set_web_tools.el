6317c3c3483d2d37465f47778cc01df7f36bf456
Luisa Lima <luisamoyalima@gmail.com>
2016-06-08 08:59:30 +0100

    An attempt at adding a proper reacjs jsx config.

--- emacs/mine/set_web_tools.el
+++ emacs/mine/set_web_tools.el
@@ -1,4 +1,10 @@
-(require 'web-mode)
+(setq js-indent-level 2)
+(setq javascript-indent-level 2)
+
+;; web mode and web mode linters...
+;; some inspiration from http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+;; and some other places
+
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
@@ -6,12 +12,39 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 (setq web-mode-code-indent-offset 2)
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-current-element-highlight t)
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
 (provide 'set_web_tools)
 ;;; set_web_tools.el ends here
