6cf4c5edeae52ec8144d38a70f4efdc1a5294541
Luisa Lima <luisalima@users.noreply.github.com>
2016-10-03 00:43:40 +0100

    Add wc-count to emacs

--- emacs/mine/set_misc.el
+++ emacs/mine/set_misc.el
@@ -1,6 +1,18 @@
 (require 'ido)
 (ido-mode t)
 
+;; show column number
+(setq column-number-mode t)
+
+;; word-count mode
+(require 'wc-mode)
+(global-set-key "\C-cw" 'wc-mode)
+
+(define-globalized-minor-mode my-global-wc-mode wc-mode
+  (lambda () (wc-mode 1)))
+
+(my-global-wc-mode 1)
+
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
