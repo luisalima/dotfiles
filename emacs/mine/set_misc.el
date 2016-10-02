87f463094ee191855bd7919fa47936733c8da1a7
Luisa Lima <luisalima@users.noreply.github.com>
2016-10-02 23:18:01 +0100

    Adds markdown to emacs

--- emacs/mine/set_misc.el
+++ emacs/mine/set_misc.el
@@ -1,4 +1,13 @@
 (require 'ido)
 (ido-mode t)
 
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "gfm-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
 (provide 'set_misc)
