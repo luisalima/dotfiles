bd38885249feb79a0045512b639bbe40e680775a
Luisa Lima <luisalima@users.noreply.github.com>
2016-10-23 11:15:30 +0100

    Adds more binaries to install.

--- install/install_binaries.sh
+++ install/install_binaries.sh
@@ -1,6 +1,6 @@
 fancy_echo "installing other binaries..."
 
-BINARIES=('google-chrome' slack sourcetree gpgtools sketch rescuetime macdown dropbox whatsapp postman skype flux)
+BINARIES=('google-chrome' slack sourcetree gpgtools sketch rescuetime macdown dropbox whatsapp postman skype flux kindle send-to-kindle evernote)
 for BINARY in "${BINARIES[@]}"
 do
     brew cask install $BINARY
