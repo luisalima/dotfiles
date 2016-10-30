2a77c6b3331517de5487b5fba616ad2708e51949
Luisa Lima <luisalima@users.noreply.github.com>
2016-10-30 11:14:55 +0000

    Better flycheck customization

--- emacs/mine/set_linter_tools.el
+++ emacs/mine/set_linter_tools.el
@@ -1,5 +1,9 @@
 (require 'flycheck)
-(global-flycheck-mode)
+;; (global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
 
 (provide 'set_linter_tools)
 ;;; set_linter_tools.el ends here
