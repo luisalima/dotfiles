e141e4676852c576fdc507c71318cd7808c198c7
Luisa Lima <luisalima@users.noreply.github.com>
2016-10-30 11:16:58 +0000

    Properly configure javascript jsx and linters at last :dance:

--- emacs/mine/set_web_tools.el
+++ emacs/mine/set_web_tools.el
@@ -5,6 +5,10 @@
 ;; some inspiration from http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
 ;; and some other places
 
+(require 'web-mode)
+(require 'js2-mode)
+(require 'json-mode)
+
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
@@ -23,10 +27,25 @@
 (add-hook 'js-mode-hook 'js2-minor-mode)
 
 ;; use eslint with web-mode for jsx files
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 
-;; customize flycheck temp file prefix
-(setq-default flycheck-temp-prefix ".flycheck")
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 ;; https://github.com/purcell/exec-path-from-shell
 ;; only need exec-path-from-shell on OSX
