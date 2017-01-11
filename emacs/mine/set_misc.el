49739c83668b72e5a8a3985e8730e32e61748818
Luisa Lima <luisalima@users.noreply.github.com>
2017-01-11 12:30:25 +0000

    Setup copying text from macos to system clipboard

--- emacs/mine/set_misc.el
+++ emacs/mine/set_misc.el
@@ -22,4 +22,17 @@
    "Major mode for editing GitHub Flavored Markdown files" t)
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
+;; http://emacs.stackexchange.com/questions/10900/copy-text-from-emacs-to-os-x-clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 (provide 'set_misc)
