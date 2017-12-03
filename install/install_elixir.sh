71738b8a38772ddef0db5ad14e5f332718fb616b
Luisa Lima <luisalima@users.noreply.github.com>
2017-12-03 21:22:06 +0000

    Do not install unecessary elixir stuff

--- install/install_elixir.sh
+++ install/install_elixir.sh
@@ -3,5 +3,5 @@
 fancy_echo "installing erlang/elixir..."
 
 brew_install elixir
-mix local.hex
-mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+mix local.hex --force --if-missing
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez --force
