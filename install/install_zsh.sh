6b8040896b3c8647501b7a04030f3a3db015c1d3
Luisa Lima <luisalima@users.noreply.github.com>
2017-12-03 21:23:29 +0000

    Do not change shells if it is already zsh

--- install/install_zsh.sh
+++ install/install_zsh.sh
@@ -4,4 +4,6 @@ fancy_echo "installing zsh and pure-prompt..."
 
 brew_install zsh zsh-completions
 npm install --global pure-prompt
-chsh -s /bin/zsh
+if [ ! "$SHELL" = '/bin/zsh' ]; then
+    chsh -s /bin/zsh
+fi
